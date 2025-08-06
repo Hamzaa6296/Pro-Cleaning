e7e88707313d1105a24fe59eb0ff28c58ee2f1bd
hamza <ah9132517@gmail.com>
2025-08-06 13:27:19 -0700

    pro cleaning landing page

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+const Navbar = () => {
+  return (
+    <>
+        <h1 className='navbar'> Navbar </h1>
+        <p> This is paragraph </p>
+    </>
+  )
+}
+
+export default Navbar
